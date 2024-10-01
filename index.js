9e62ea66b8ce98a33f12dc73171c16c47a226c40
Gabriel Souza <92094301+gabrielsl11@users.noreply.github.com>
2024-10-01 11:30:55 -0300

    Update

--- index.js
+++ index.js
@@ -3,8 +3,11 @@
 ////////////////////////////
 
 const { engine } = require('express-handlebars')
+const fileUpload = require('express-fileupload')
 const express = require('express')
 const mysql = require('mysql2')
+// const path = require('path')
+const fs = require('fs')
 const app = express()
 
 ///////////////////
@@ -16,7 +19,7 @@ const conn = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '1234',
-    database: 'dbexample'
+    database: 'shopping'
 })
 
 // Verificação de conexão
@@ -33,19 +36,69 @@ app.engine('handlebars', engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.set('views', './views')
 
-// Configuração do Middleware para análise de requisições
+// Manipulação de requisições (Middleware)
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 // Configuração para arquivos estáticos
 app.use(express.static('public'))
 
+// Referenciando o diretório de imagens
+app.use('/images', express.static('./images'))
+// Habilitando o upload de arquivos
+app.use(fileUpload())
+
 ///////////
 // Rotas //
 ///////////
 
 app.get('/', (req, res) => {
-    res.render('home')
+    let sql = 'SELECT * FROM products ORDER BY id DESC;'
+    conn.query(sql, (err, ok) => {
+        res.render('home', { products: ok })
+    })
+
+})
+
+app.post('/register', (req, res) => {
+    // console.log(req.body)
+    // console.log(req.files.image.name)
+    // res.end()
+    let name = req.body.name
+    let price = req.body.price
+    let image = req.files.image.name
+
+    let sql = `INSERT INTO products (name, price, image) VALUES ('${name}', '${price}', '${image}');`
+
+    // Somente o 'conn.query(sql)' funcionaria para inserção de novos registros.
+    conn.query(sql, (err, ok) => {
+        if (err) {
+            throw err
+        } else {
+            req.files.image.mv(__dirname + '/images/' + req.files.image.name)
+            console.log(ok)
+        }
+    })
+
+    res.redirect('/')
+})
+
+app.get('/delete/:id&:image', (req, res) => {
+    // console.log(req.params.id)
+    // console.log(req.params.image)
+    // res.end()
+    let sql = `DELETE FROM products WHERE id = ${req.params.id}`
+    conn.query(sql, (err, ok) => {
+        if (err) {
+            throw err
+        } else {
+            fs.unlink(__dirname + '/images/' + req.params.image, (err_img) => {
+                console.log('Could not remove image!')
+            })
+        }
+    })
+
+    res.redirect('/')
 })
 
 //////////////
